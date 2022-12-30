40a20fa9552ae150bdd71a5a9b60429d6d73d675
Hafiz <hafizmehdiyev333@gmail.com>
2022-12-30 17:52:11 +0400

    Button Puzzle Mechanics [Done]

--- FINAL/Assets/Scripts/PuzzleManager.cs
+++ FINAL/Assets/Scripts/PuzzleManager.cs
@@ -12,6 +12,19 @@ public class PuzzleManager : MonoBehaviour
     Sequence sequenceBlade;
 
 
+    //*** Button ***
+    bool button1 = false;
+    bool button2 = false;
+    bool button3 = false;
+    int PressCount = 3;
+
+
+
+
+
+    public List<GameObject> buttonList;
+
+
     private void Awake()
     {
         if (instance == null)
@@ -45,7 +58,54 @@ public class PuzzleManager : MonoBehaviour
     }
     public void ButtonPress(GameObject Button)
     {
+        buttonList.Add(Button);
+        Button.gameObject.layer = 1;
+        PressCount--;
         Button.transform.DOMoveY(Button.transform.position.y - .12f, .5f);
+        if (Button.gameObject.name == "Stone Button 1")
+        {
+            Debug.Log(Button.gameObject.name);
+            button1 = true;
+        }
+
+        if (Button.gameObject.name == "Stone Button 3" && button1)
+        {
+            Debug.Log(Button.gameObject.name);
+            button3 = true;
+        }
+
+        if (Button.gameObject.name == "Stone Button 2" && button3)
+        {
+            Debug.Log(Button.gameObject.name);
+            button2 = true;
+        }
+        if (button1 && button2 && button3)
+        {
+            Debug.Log("UNLOCKED");
+        }
+
+        if (PressCount <= 0 && !button2)
+        {
+            Debug.Log("LOCKED");
+            button1 = false;
+            button2 = false;
+            button3 = false;
+
+            var seq = DOTween.Sequence();
+            seq.AppendInterval(1f).OnComplete(() =>
+            {
+                PressCount = 3;
+
+
+
+                foreach (GameObject ButtonsInList in buttonList)
+                {
+                    ButtonsInList.transform.DOMoveY(Button.transform.position.y + .12f, .5f);
+                }
+
+            });
+
+        }
     }
 
 }
